26d120cda0d33497ecc09ffe5407b598416f0298
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-07-17 14:28:13 +0530

    File changed

--- src/SumOfMatrix.java
+++ src/SumOfMatrix.java
@@ -3,31 +3,33 @@ import java.util.Scanner;
 public class SumOfMatrix {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-
+// initialization of variables:
         int[][] arrayOne = new int[2][2];
-        int[][] arraytwo = new int[2][2];
+        int[][] arrayTwo = new int[2][2];
         int[][] sumArray = new int[2][2];
-        System.out.println("first array elemts");
+
+//        Taking Input From User for both Array
+        System.out.println("first array elements");
         for (int i = 0;i < 2; i++){
             for (int j = 0; j<2; j++){
                 arrayOne[i][j] = sc.nextInt();
             }
         }
-        System.out.println("second array elemts");
+        System.out.println("second array elements");
         for (int i = 0;i < 2; i++){
             for (int j = 0; j<2; j++){
-                arraytwo[i][j] = sc.nextInt();
+                arrayTwo[i][j] = sc.nextInt();
             }
         }
 
-
+// Performing Sum Of Array
         for (int i = 0; i<2; i++){
             for (int j = 0; j<2; j++){
-                sumArray[i][j] = arrayOne[i][j] + arraytwo[i][j];
+                sumArray[i][j] = arrayOne[i][j] + arrayTwo[i][j];
             }
         }
 
-
+// First Array print
         System.out.println("FIRST ARRAY");
         for (int i = 0; i<2; i++){
             for (int j = 0; j<2; j++){
@@ -36,14 +38,16 @@ public class SumOfMatrix {
             System.out.println();
         }
 
+// First Array print
         System.out.println("SECOND ARRAY");
         for (int i = 0; i<2; i++){
             for (int j = 0; j<2; j++){
-                System.out.print(arraytwo[i][j]+" ");
+                System.out.print(arrayTwo[i][j]+" ");
             }
             System.out.println();
         }
 
+// Print Sum of arrays
         System.out.println("SUM OF ARRAY");
         for (int i = 0; i<2; i++){
             for (int j = 0; j<2; j++){
@@ -51,9 +55,5 @@ public class SumOfMatrix {
             }
             System.out.println();
         }
-
-
-
-
     }
 }
