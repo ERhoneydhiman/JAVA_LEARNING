00552510e21bd28c60457b95c25f773fe86e14cb
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-07-21 15:20:37 +0530

    creating object based on user input..

---
+++ src/OOPS/UserGivesObjectDetails.java
@@ -0,0 +1,36 @@
+package OOPS;
+
+import java.util.Scanner;
+
+public class UserGivesObjectDetails {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number of Students: ");
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+
+        for (int i = 0; i<n ; i++){
+            System.out.println("Enter Details of Student" + i);
+            System.out.println("Enter ID of student: ");
+            int id = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("Enter NAME of student: ");
+            String name = sc.nextLine();
+
+            System.out.println("Enter COURSE of student: ");
+            String course = sc.nextLine();
+
+            System.out.println("Enter FEES of student: ");
+            Double fees = sc.nextDouble();
+
+            students[i] = new Student(id, name, course, fees);
+        }
+
+        System.out.println("Details of Students are...: ");
+
+        for (Student student: students){
+            student.displayInfo();
+        }
+    }
+}
